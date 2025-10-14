2d5e61cdaa151c0f23230390bb3ae52f8cc19093
ToprakM <67737840+ToprakMeric@users.noreply.github.com>
2025-10-14 12:55:14 +0300

    Update username

--- user-config.py
+++ user-config.py
@@ -1,6 +1,6 @@
 mylang = 'tr'
 family = 'wikipedia'
-usernames['wikipedia']['tr'] = 'ToprakBot'
+usernames['wikipedia']['tr'] = 'ToprakBot 2'
 password_file = "user-password.py"
 maxthrottle = 0
 put_throttle = 0
